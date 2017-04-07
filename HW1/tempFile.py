cd16d0916d73f7da435e414c9e45718659b07081
minhncsocial <minhnc.social@gmail.com>
2017-04-08 07:40:58 +0800

    just change file to send vipul

--- HW1/tempFile.py
+++ HW1/tempFile.py
@@ -5,12 +5,34 @@ GOLDEN_RATE = 0.3819660113; #(3 - sqrt(5)) / 2
 def fx(x):
 	return x**4 - 14*x**3 + 60*x**2 - 70*x;
 
+# Define function calculating sum of a range [1, number]
+def sumRange(number):
+	sum = 0;
+	value = 1;
+	while value <= number:
+		sum += value;
+		value += 1;
+
+	return sum;
+
 # Define absolute function
 def abs(number):
 	if number < 0:
 		return -number;
 	else:
 		return number;
+
+# Define function measuring number of Interations to get result error < epsilon
+def measureGoldenSectionIteration(epsilon):
+	N = 1;
+	temp = GOLDEN_RATE;
+	threshold = epsilon/2;
+
+	while temp > threshold:
+		temp *= GOLDEN_RATE;
+		N = N + 1;
+
+	return N;
 
 # Define function solve function func in range [startValue, endValue], result error is less than epsion
 def minFGoldenSection(func, startValue, endValue, epsilon):
